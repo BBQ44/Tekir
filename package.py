03a7f97de9adc5010ae744c04f88be4190e23237
BBQ44 <42846778+BBQ44@users.noreply.github.com>
2018-08-30 21:20:29 +0300

    Create package.py

---
+++ package.py
@@ -0,0 +1,14 @@
+
+  "name": "tekirabla",
+  "version": "0.0.1",
+  "description": "Tekir Abla...",
+  "main": "tekir.py",
+  "scripts": 
+    "test": "node bot.js"
+
+  "author": "Hasan",
+  "license": "ISC",
+  "dependencies": 
+    "discord.js": "^11.3.2"
+  
+
